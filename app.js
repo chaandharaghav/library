01f801a08f7ffc7037a03bd027d2866bc6e594e9
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-07-30 19:45:13 +0530

    Write function to insert the data from form submission into myLibrary array and clear form on success

--- app.js
+++ app.js
@@ -20,6 +20,31 @@ newBookForm.addEventListener("submit", function (e) {
   const title = e.target[0].value;
   const author = e.target[1].value;
   const numPages = e.target[2].value;
-
-  console.log(title, author, numPages);
+  if (!bookExists(title)) {
+    addNewBook(title, author, numPages);
+    clearForm();
+  } else {
+    alert("You've already entered this book");
+  }
 });
+
+const bookExists = function (bookTitle) {
+  bookTitle = bookTitle.toLowerCase();
+  return myLibrary.some((book) => book.title.toLowerCase() === bookTitle);
+};
+
+const clearForm = function () {
+  newBookForm[0].value = "";
+  newBookForm[1].value = "";
+  newBookForm[2].value = "";
+};
+
+const addNewBook = function (title, author, numPages) {
+  const newBook = new Book(title, author, numPages);
+
+  addBookToLibrary(newBook);
+};
+
+const addBookToLibrary = function (book) {
+  myLibrary.push(book);
+};
